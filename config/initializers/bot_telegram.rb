cb1b112a56a68d437e02ce6a780bdb89f1dcddf6
Santiago Ramos <sramos@oei.es>
2019-07-12 13:52:31 +0200

    Configuracion de telegram

---
+++ config/initializers/bot_telegram.rb
@@ -0,0 +1,6 @@
+Telegram.bots_config = {
+  default: {
+    token: ENV['TELEGRAM_TOKEN']||"TOKEN",
+    username: ENV['TELEGRAM_USERNAME']||"botname"
+  },
+}
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-admin = Admin.new( name: 'Default Admin',
-                   email: 'admin@admin.es',
-                   password: '123456789',
-                   password_confirmation: '123456789'
-               )
-admin.skip_confirmation!
-admin.save!
+unless Admin.any?
+  admin = Admin.new( name: 'Default Admin',
+                     email: 'admin@admin.es',
+                     password: '123456789',
+                     password_confirmation: '123456789'
+                 )
+  admin.skip_confirmation!
+  admin.save!
+end
